5fc2425bf6336d5418ae40abc46885f336b5102e
Kushan Fernando <Kushan.Fernando@trustwave.com>
2023-10-09 00:52:53 +1300

    mostly working except +- buttons

--- card.js
+++ card.js
@@ -3,6 +3,7 @@ import {
     html,
     css,
 } from "https://unpkg.com/lit-element@2.0.1/lit-element.js?module";
+import { until } from 'https://unpkg.com/lit-html@2.0.1/directives/until.js';
 
 class WaterParamStatsCard extends LitElement {
     static get properties() {
@@ -11,131 +12,28 @@ class WaterParamStatsCard extends LitElement {
             config: undefined,
             stateObj: undefined,
             popup: false,
-            newValue: undefined,
-            originalValue: undefined
+            newEntryEnabled: false,
+            newEntryEntityId: undefined,
+            newEntryOriginalValue: undefined,
+            newEntryCurrentValue: undefined,
+            html: undefined
         };
     }
 
     // required
     setConfig(config) {
         this.config = config;
-    }
 
-    openPopup() {
-        this.popup = true;
-    }
+        if (this.newEntry === undefined)
+            this.newEntry = {};
 
-    cancelPopup() {
-        this.newValue = this.originalValue;
-        this.popup = false;
-    }
-
-    getStep() {
-        return parseFloat(this.hass.states["input_number.calcium_reading"].attributes.step || 1);
-    }
-
-    newValueIncrement() {
-        console.log("Incrementing " + this.newValue);
-        this.setNewValue(this.newValue + this.getStep());
-    }
-
-    newValueDecrement() {
-        console.log("Decrementing " + this.newValue);        
-        this.setNewValue(this.newValue - this.getStep());
-    }
-
-    get value() {
-        return this.newValue;
-    }
-
-    set value(value) {
-        const oldValue = this.value;
-        this.newValue = value;
-        this.requestUpdate('value', oldValue);
-    }
-
-    newValueChange(e) {
-        this.newValue = e.target.value;
-    }
-
-    setNewValue(newVal) {
-        this.newValue = newVal;
-        this.requestUpdate('value', this.newValue);
-    }
-
-    onNewValueChange(e) {
-        console.log("onNewValueChange to " + e.target.value);
-        this.newValue = parseFloat(e.target.value);
-    }
-
-    async submitReading() {
-        try {
-            await this.hass.callService('input_number', 'set_value', {
-                entity_id: 'input_number.calcium_reading',
-                value: '15.0'
-            });
-        } catch (error) {
-            console.error('Error calling service:', error);
-        }
-        this.popup = false;
-    }
-
-    getPopupWindow() {
-        const _newvalue = this.config.new_value || false;
-        if (_newvalue) {
-            return html`<tr>
-                           <td colspan=2 style='text-align: right;'>
-                             <button class="flat-button" @click="${this.openPopup}">Record New Value</button>
-                             <div class="overlay"></div>
-                             <ha-card class="popup">
-                               <div class="input-row">
-                                 <label for="value">New ${this.config.title} Reading:</label>
-                                 <button class="control-button" @click="${this.newValueDecrement}"><ha-icon icon="mdi:minus"></ha-icon></button>
-                                 <input type="number" class="value-input" step="0.01" value='${this.value}' @change=${this.onNewValueChange}/>
-                                 <button class="control-button" @click="${this.newValueIncrement}"><ha-icon icon="mdi:plus"></ha-icon></button>
-                                 <span id="unit">ppm</span>
-                               </div>
-                               <div class="button-container">
-                                 <button class="flat-button flat-button-secondary" @click="${this.cancelPopup}">Cancel</button>
-                                 <button class="flat-button" @click="${this.submitReading}">Record New Value</button>
-                               </div>
-                             </ha-card>
-                           </td>
-                        </tr>`;
+        const _newEntry = this.config.new_entry;
+        this.newEntryEnabled = _newEntry?.enabled || false;
+        if (this.newEntryEnabled) {
+            this.newEntryEntityId = _newEntry.entity;
         }
 
-        return html``;
-    }
-
-    render() {
-        const _title = this.config.title || "Water Parameter";
-        
-        return html`
-           <ha-card header="${_title}">
-             <div class="card-content">
-               <table style='width: 100%'>
-                 ${this.config.stats.map((stat) => {
-                     const _readonly = stat.readonly || false;
-
-                     return html`<tr>
-                                    <td class='td-field ${_readonly ? `readonly` : ``}'>${stat.title}</td>
-                                    <td class='td-value ${_readonly ? `readonly` : ``}'>${stat.value}</td>
-                                 </tr>`
-                     })
-                 }
-                 <tr>
-                   <td colspan=2>
-                     <div class='chart-container'>
-                       <iframe class="chart-frame chart-frame-first" src="${this.config.charts.guage}"></iframe>
-                       <iframe class="chart-frame chart-frame-second" src="${this.config.charts.timeseries}"></iframe>
-                     </div>
-                   </td>
-                 </tr>
-                 ${this.getPopupWindow() }                 
-               </table>               
-             </div>
-           </ha-card>
-         `;
+        this.renderHtmlAsync();
     }
 
     updated(changedProperties) {
@@ -149,10 +47,204 @@ class WaterParamStatsCard extends LitElement {
             }
         }
         else if (changedProperties.has('hass')) {
-            this.newValue = parseFloat(this.hass.states["input_number.calcium_reading"].state);
-            this.originalValue = this.newValue;
+
+            if (this.newEntryEnabled) {
+                this.newEntryCurrentValue = parseFloat(this.hass.states[this.newEntryEntityId].state)
+                this.newEntryOriginalValue = this.newEntryCurrentValue;
+            }
+
+            this.renderHtmlAsync();
         }
     }
+
+    openPopup() {
+        this.popup = true;
+    }
+
+    cancelPopup() {
+        this.newEntryCurrentValue = this.newEntryOriginalValue;
+        this.popup = false;
+    }
+
+    getStep() {
+        return parseFloat(this.hass.states[this.newEntryEntityId].attributes.step || 1);
+    }
+
+    newValueIncrement() {
+        this.setNewValue(this.newEntryCurrentValue + this.getStep());
+    }
+
+    newValueDecrement() {
+        this.setNewValue(this.newEntryCurrentValue - this.getStep());
+    }
+
+    get value() {
+        if (this.newEntry == undefined || !this.newEntryEnabled)
+            return 0;
+
+        return this.newEntryCurrentValue;
+    }
+
+    set value(value) {
+        const oldValue = this.value;
+        this.newEntryCurrentValue = value;
+        this.requestUpdate('value', oldValue);
+    }
+
+    isTemplate(value) {
+        return value?.includes("{{");
+    }
+
+    newValueChange(e) {
+        this.newEntryCurrentValue = e.target.value;
+    }
+
+    setNewValue(newVal) {
+        this.newEntryCurrentValue = newVal;
+        this.requestUpdate('value', this.newEntryCurrentValue);
+    }
+
+    onNewValueChange(e) {
+        this.newEntryCurrentValue = parseFloat(e.target.value);
+    }
+
+    async submitReading() {
+        try {
+            await this.hass.callService('input_number', 'set_value', {
+                entity_id: this.newEntryEntityId,
+                value: this.newEntryCurrentValue
+            });
+        } catch (error) {
+            console.error('Error calling service:', error);
+        }
+        this.popup = false;
+    }
+
+    getPopupWindow() {
+        if (this.newEntryEnabled) {
+            const _newEntry = this.config.new_entry;
+
+            const _buttonText = _newEntry.button_text || "Record New Value"
+            const _submitText = _newEntry.submit_text || "Record New Value"
+            const _cancelText = _newEntry.cancel_text || "Cancel";
+            const _uom = this.hass.states[this.newEntryEntityId].attributes.unit_of_measurement || "";
+
+            return html`<tr>
+                           <td colspan=2 style='text-align: right;'>
+                             <button class="flat-button" @click="${this.openPopup}">${_buttonText}</button>
+                             <div class="overlay"></div>
+                             <ha-card class="popup">
+                               <div class="input-row">
+                                 <label for="value">New ${this.config.title} Reading:</label>
+                                 <button class="control-button" @click="${this.newValueDecrement}"><ha-icon icon="mdi:minus"></ha-icon></button>
+                                 <input type="number" class="value-input" step="${this.getStep() }" .value='${this.value}' @change=${this.onNewValueChange}/>
+                                 <button class="control-button" @click="${this.newValueIncrement}"><ha-icon icon="mdi:plus"></ha-icon></button>
+                                 <span id="unit">${_uom}</span>
+                               </div>
+                               <div class="button-container">
+                                 <button class="flat-button flat-button-secondary" @click="${this.cancelPopup}">${_cancelText}</button>
+                                 <button class="flat-button" @click="${this.submitReading}">${_submitText}</button>
+                               </div>
+                             </ha-card>
+                           </td>
+                        </tr>`;
+        }
+
+        return html``;
+    }
+
+    validateConfig() {
+        if (this.hass === undefined)
+            return { is_valid: false, error: "Loading..." };
+
+        if (this.newEntryEnabled) {
+            if (this.newEntryEntityId === undefined)
+                return { is_valid: false, error: "Entity must be specified for the new entry option." };
+
+            const entity = this.hass.entities[this.newEntryEntityId];
+
+            if (entity === undefined)
+                return { is_valid: false, error: "Entity id '" + this.newEntryEntityId +"' is not found." };
+
+            if (entity.platform != 'input_number')
+                return { is_valid: false, error: "Only input_numbers are supported for new entry and entity id '" + this.newEntryEntityId + "' is not an input_number." };
+        }
+
+        return { is_valid: true };
+    }
+
+    async waitUntil(condition, time = 100) {
+        while (!condition()) {
+            await new Promise((resolve) => setTimeout(resolve, time));
+        }
+    }
+
+    async getHtmlAsync() {
+        const _title = this.config.title || "Water Parameter";
+        let _statsHtml = html``;
+
+        for (var idx = 0; idx < this.config.stats.length; idx++) {
+            const stat = this.config.stats[idx];
+            const _readonly = stat.readonly || false;
+
+            this._value = stat.value;
+
+            if (this.isTemplate(this._value)) {
+                this.ready = false;
+
+                await this.hass.connection.subscribeMessage((msg) => {
+                    this._value = msg.result;
+                    this.ready = true;
+                }, { type: "render_template", template: this._value });
+
+                await this.waitUntil(() => this.ready === true);
+            }
+
+            _statsHtml = html`${_statsHtml}
+                        <tr>
+                           <td class='td-field ${_readonly ? `readonly` : ``}'>${stat.title}</td>
+                           <td class='td-value ${_readonly ? `readonly` : ``}'>${this._value}</td>
+                        </tr>`;
+		}
+
+        return html`
+           <ha-card header="${_title}">
+             <div class="card-content">
+               <table style='width: 100%'>
+                 ${_statsHtml}
+                 <tr>
+                   <td colspan=2>
+                     <div class='chart-container'>
+                       <iframe class="chart-frame chart-frame-first" src="${this.config.charts.guage}"></iframe>
+                       <iframe class="chart-frame chart-frame-second" src="${this.config.charts.timeseries}"></iframe>
+                     </div>
+                   </td>
+                 </tr>
+                 ${this.getPopupWindow()}                 
+               </table>               
+             </div>
+           </ha-card>
+         `;
+    }
+
+    getErrorHtml(error) {
+        return html`<ha-alert alert-type="error">${error}</ha-alert>`;
+    }
+
+    async renderHtmlAsync() {
+        this.html = html`<span>Loading...</span>`
+
+        const validation = this.validateConfig();
+
+        if (!validation.is_valid)
+            this.html = this.getErrorHtml(validation.error);
+        else
+            this.html = await this.getHtmlAsync();
+
+        this.requestUpdate();
+    }
+
+    render() { return this.html; }
 
     static get styles() {
         return css`
@@ -282,6 +374,7 @@ class WaterParamStatsCard extends LitElement {
               justify-content: flex-end;
               align-items: center;
               margin-bottom: 20px;
+              margin-left: 35px;
           }
 
           .input-row label {
@@ -302,7 +395,7 @@ class WaterParamStatsCard extends LitElement {
 
           .value-input {
             padding: 5px;
-            width: 25%;
+            width: 50px;
             border: 0px solid #ccc;
             border-radius: 0px;
             font-size: 16px;
